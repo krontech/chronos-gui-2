c5d4f648b3c0609c24ebf7e41a9d5fd1f1059447
David Roberts <ddr@krontech.ca>
2018-06-26 21:06:39 -0700

    Open Designer in such a way that it defaults to the right save location for new screens.

--- util/designer.sh
+++ util/designer.sh
@@ -1,9 +1,13 @@
-if [ -d src/components ]; then
-	PYQTDESIGNERPATH=src/components designer
+#!/bin/bash
+
+if [ -d src/widgets ]; then
+	cd src/screens #set default qt designer save location
+	PYQTDESIGNERPATH=../widgets designer
 else 
-	if [ -d ../src/components ]; then
-		PYQTDESIGNERPATH=../src/components designer
+	if [ -d ../src/widgets ]; then
+		cd ../src/screens
+		PYQTDESIGNERPATH=../widgets designer
 	else
-		xmessage -nearmouse "Error: Couldn't find ~chronos-gui-2/src/components/."
+		xmessage -nearmouse "Error: Couldn't find ~chronos-gui-2/src/widgets/."
 	fi
 fi
