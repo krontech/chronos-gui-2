d9f8b3f389976c74f588ce052207859e7b64edb7
David Roberts <ddr@krontech.ca>
2018-07-04 16:44:26 -0700

    Initial unrefined customization of check box, spin box, radio button, and scroll bar for the Chronos.

---
+++ src/widgets/spin_box.py
@@ -0,0 +1,34 @@
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+from debugger import *; dbg
+
+from touch_margin_object import TouchMarginObject, MarginWidth
+
+
+class SpinBox(QSpinBox, TouchMarginObject):
+	Q_ENUMS(MarginWidth) #This is needed here. I don't know why the definition in the TouchMarginObject doesn't work.
+	
+	def __init__(self, parent=None, inEditor=False):
+		super().__init__(parent, inEditor=inEditor)
+		
+		if self.inEditor:
+			self.setStyleSheet(f"""
+				/* Editor style. Use border to show were click margin is, so we don't mess it up during layout. */
+				font-size: 16px;
+				background: white;
+			""" + self.styleSheet())
+		else:
+			self.setStyleSheet(f"""
+				/* App style. Use margin to provide further click area outside the visual button. */
+				font-size: 16px;
+				background: white;
+				border: 1px solid black;
+				border-left-color: rgb(50,50,50);
+				border-top-color: rgb(50,50,50); /* Add a subtle 3d-ness until we figure out drop-shadows. */
+			""" + self.styleSheet())
+
+
+	def sizeHint(self):
+		return QSize(181, 81)
