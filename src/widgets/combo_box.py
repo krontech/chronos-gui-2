10cc459fbb152b05801796715d39a74a39d9f2ee
David Roberts <ddr@krontech.ca>
2018-07-06 13:43:02 -0700

    Added missing combo box widget.

---
+++ src/widgets/combo_box.py
@@ -0,0 +1,49 @@
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+from debugger import *; dbg
+
+from touch_margin_plugin import TouchMarginPlugin, MarginWidth
+
+
+class ComboBox(QComboBox, TouchMarginPlugin):
+	Q_ENUMS(MarginWidth) #This is needed here. I don't know why the definition in the TouchMarginPlugin doesn't work.
+	
+	def __init__(self, parent=None, inEditor=False):
+		super().__init__(parent, inEditor=inEditor)
+		self.refreshStyle()
+
+
+	def sizeHint(self):
+		return QSize(181, 81)
+	
+	
+	def refreshStyle(self):
+		if self.inEditor:
+			self.setStyleSheet(f"""
+				/* Editor style. Use border to show were click margin is, so we don't mess it up during layout. */
+				font-size: 16px;
+				background: white;
+				
+				/* use borders instead of margins so we can see what we're doing */
+				border-left:   {self.clickMarginLeft   * 10 + 1}px solid {self._clickMarginColor};
+				border-right:  {self.clickMarginRight  * 10 + 1}px solid {self._clickMarginColor};
+				border-top:    {self.clickMarginTop    * 10 + 1}px solid {self._clickMarginColor};
+				border-bottom: {self.clickMarginBottom * 10 + 1}px solid {self._clickMarginColor};
+			""" + self.originalStyleSheet())
+		else:
+			self.setStyleSheet(f"""
+				/* App style. Use margin to provide further click area outside the visual button. */
+				font-size: 16px;
+				background: white;
+				border: 1px solid black;
+				border-left-color: rgb(50,50,50);
+				border-top-color: rgb(50,50,50); /* Add a subtle 3d-ness until we figure out drop-shadows. */
+				
+				/* Add some touch space so this widget is easier to press. */
+				margin-left: {self.clickMarginLeft*10}px;
+				margin-right: {self.clickMarginRight*10}px;
+				margin-top: {self.clickMarginTop*10}px;
+				margin-bottom: {self.clickMarginBottom*10}px;
+			""" + self.originalStyleSheet())
---
+++ src/widgets/combo_box_plugin.py
@@ -0,0 +1,48 @@
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtDesigner import QPyDesignerCustomWidgetPlugin
+
+from combo_box import ComboBox
+
+
+class ComboBoxPlugin(QPyDesignerCustomWidgetPlugin):
+
+	def __init__(self, parent=None):
+		super().__init__(parent)
+
+		self.initialized = False
+
+	def initialize(self, core):
+		if not self.initialized:
+			self.initialized = True
+
+	def isInitialized(self):
+		return self.initialized
+
+	def createWidget(self, parent):
+		return ComboBox(parent, inEditor=True)
+
+	def name(self):
+		return "ComboBox"
+
+	def group(self):
+		return "Chronos"
+
+	def icon(self):
+		return QIcon(QPixmap("../../assets/images/qt_creator_icons/button.svg"))
+
+	def toolTip(self):
+		return """A drop-down with adjustable margins.
+		
+Margins are displayed in Qt Designer so we can work with them. They
+are hidden in the app itself, but they are still clickable. This is
+important to maximize touch area, which makes the combo box much
+more clickable."""
+
+	def whatsThis(self):
+		return self.toolTip() # ¯\_(ツ)_/¯ I have no idea how to trigger this.
+
+	def isContainer(self):
+		return False
+
+	def includeFile(self):
+		return "combo_box"
