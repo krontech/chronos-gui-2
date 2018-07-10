ba0cd48069b646bf7ffcfcd26139e1db2a0648c2
David Roberts <ddr@krontech.ca>
2018-07-10 15:33:04 -0700

    Tweaked slider to feel more "natural".

--- src/widgets/slider.py
+++ src/widgets/slider.py
@@ -32,14 +32,14 @@ class Slider(QSlider):
 				image: url({"../../" if self.inEditor else ""}assets/images/handle-bars-41x81+20.png); /* File name fields: width x height + vertical padding. */
 				background: {self.clickMarginColorHandle if self.inEditor or drawDebugArea else "transparent"};
 				/*Setting a border here messes up scrolling; scrolling goes faster than the drag does. Something in the math is off. */
-				margin: -35px -200px; /* y: -slider groove margin. x: touch padding outsidet the groove. Clipped by Slider width. Should be enough for most customizations if we move stuff around. */
+				margin: -20px -200px; /* y: -slider groove margin. x: touch padding outsidet the groove. Clipped by Slider width. Should be enough for most customizations if we move stuff around. */
 				padding: -1px;
 			}}
 			Slider::groove:vertical {{
 				/* This slider has an extra-large hitbox, so it's easy to tap. This is done by giving it an invisible margin. A small graphic, handle-bars.png, draws the visual slider for us. Note that the real area of the slider, and it's QWidget holders, must be as large as the hitbox.*/
 				border: 1px solid black;
 				width: 15px; /* The groove expands to the size of the slider by default. We'll give it an explicit width. */
-				margin: 35px; /* Handle needs margin to compensate for this. Controls how far from the bottom and top the groove is. */
+				margin: 20px; /* Handle needs margin to compensate for this. Controls how far from the bottom and top the groove is. */
 				border-radius: 7.5px; /* Half of width. More turns it off. */
 			}}
 		""" + self.styleSheet())
