8a2ee982a5a332ed953b43ade9ee4c5b9b4131da
David Roberts <robertsdavidddr@gmail.com>
2018-08-08 23:04:03 -0700

    Added a menu to the main screen.

--- src/screens/main.py
+++ src/screens/main.py
@@ -1,5 +1,5 @@
 from PyQt5 import uic, QtWidgets, QtCore
-from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtCore import pyqtSlot, QPropertyAnimation, QPoint
 
 from debugger import *; dbg
 import api_mock as api
@@ -14,6 +14,7 @@ class Main(QtWidgets.QDialog):
 		
 		# Panel init.
 		self.move(0, 0)
+		self.setFixedSize(800, 480) #hide menus, which are defined off-screen to the right
 		self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
 		self.setAttribute(QtCore.Qt.WA_TranslucentBackground, True)
 		
@@ -22,7 +23,8 @@ class Main(QtWidgets.QDialog):
 		self.uiDebugB.clicked.connect(lambda: window.show('widget_test'))
 		self.uiDebugC.clicked.connect(lambda: window.show('stamp'))
 		self.uiClose.clicked.connect(QtWidgets.QApplication.closeAllWindows)
-		self.uiTriggers.clicked.connect(lambda: window.show('triggers'))
+		#self.uiTriggers.clicked.connect(lambda: window.show('triggers'))
+		self.linkButtonToMenu(self.uiRecordingAndTriggers, self.uiRecordingAndTriggersMenu)
 		
 		# Polling-based updates.
 		# self.updateBatteryStatus()
@@ -44,7 +46,7 @@ class Main(QtWidgets.QDialog):
 		print("Analog gain is %ins." % api.get("recordingAnalogGain"))
 	
 	def updateBatteryStatus(self):
-		self.uiBatteryLevel.setText(
+		self.uiBattery.setText(
 			f"{round(api.control('get', ['batteryCharge'])['batteryCharge']*100)}%" )
 		
 	@pyqtSlot(int)
@@ -73,6 +75,67 @@ class Main(QtWidgets.QDialog):
 		step1percent = (self.uiExposureSlider.minimum() + self.uiExposureSlider.maximum()) // 100
 		self.uiExposureSlider.setPageStep(step1percent)
 		self.uiExposureSlider.setSingleStep(step1percent)
+	
+	def linkButtonToMenu(self, button, menu):
+		"""Have one of the side bar buttons bring up its menu.
+		
+		The menu closes when it looses focus.
+		"""
+		
+		paddingLeft = 20 #Can't extract this from the CSS without string parsing.
+		shownAt = QPoint(self.uiPlayAndSave.x() - menu.width() + 1, menu.y())
+		hiddenAt = QPoint(self.uiPlayAndSave.x() - paddingLeft, menu.y())
+		anim = QPropertyAnimation(menu, b"pos")
+		anim.setDuration(16*2) #framerate ms (?) * frames to animate, excluding start and end frame?
+		anim.setStartValue(hiddenAt)
+		anim.setEndValue(shownAt)
+		menu.hide()
+		
+		def toggleMenu():
+			menu.show()
+				
+			if anim.currentTime() == 0 or anim.direction() == QPropertyAnimation.Backward:
+				anim.setDirection(QPropertyAnimation.Forward)
+				menu.setFocus()
+			else:
+				anim.setDirection(QPropertyAnimation.Backward)
+				
+			if anim.state() == QPropertyAnimation.Stopped:
+				anim.start()
+		
+		self.uiRecordingAndTriggers.clicked.connect(toggleMenu)
+		
+		def hideMenu(evt):
+			if button.hasFocus():
+				# The button to toggle this menu is now focused, and will
+				# probably toggle it shut. Don't close the menu, or it would
+				# toggle it open again.
+				return
+			
+			anim.setDirection(QPropertyAnimation.Backward)
+			
+			if anim.state() == QPropertyAnimation.Stopped:
+				anim.start()
+			
+			# The original idea was animate margin left and button width, instead
+			# of the menu position, so that the buttons were always fully
+			# clickable even before they'd appeared. However, as it seems
+			# effectively impossible to change the margin of a button without
+			# going through (text-based) CSS, we just animate the whole menu. :(
+			
+			#child = menu.children()[0]
+			#margins = child.contentsMargins() #doesn't work, probably qt bug - just returns 0,0,0,0
+			#margins = child.style().subElementRect(QtWidgets.QStyle.SE_PushButtonFocusRect, QtWidgets.QStyleOptionButton(), child) #doesn't work, returns … default margins? I'm not sure what 21,21,-22,-22 is.
+			#margins = ???
+		
+		menu.focusOutEvent = hideMenu
+		
+		def animationFinished():
+			"""Hide menu when not needed."""
+			if anim.direction() == QPropertyAnimation.Backward:
+				menu.hide()
+			
+		anim.finished.connect(animationFinished)
 		
 	
 	# ~Emit to signal:
