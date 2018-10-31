acc5c239ccbd9673170a0bbbf991303e9a7e5c53
David Roberts <ddr@krontech.ca>
2018-10-30 20:49:46 -0700

    tab-based trigger screen layout

--- src/screens/triggers.py
+++ src/screens/triggers.py
@@ -10,9 +10,36 @@ settings = QtCore.QSettings('Krontech', 'back-of-camera interface')
 
 
 class Triggers(QtWidgets.QDialog):
+	"""Trigger screen. Configure one IO trigger at a time.
+	
+		This screen is slightly unusual in that triggers are only
+		applied when you hit "apply" or "done", instead of the usual
+		apply-on-change. This is because these settings change
+		electrical properties, and some configurations - such as
+		changing a 1mA pullup to a 20mA pullup - could do physical
+		damage. Having to hit another button provides some safety.
+		
+		Here are some notable variables involved:
+			- triggerCapabilities: The properties of the each
+				available trigger. Things like what type it is, what
+				pullups are available, etc. These only change between
+				models of camera, never on an individual camera.
+			- triggerConfiguration: How the triggers are set up on
+				this camera. When you hit Save or Done, this is what
+				is saved.
+			- triggerState: Which triggers are currently active. This
+				can change extremely frequently, but is only polled
+				every so often to avoid network congestion.
+		"""
 	
 	#Save current screen by ID, not by index or display text because those are UI changes.
+	#These map IDs to indexes, and must be updated when the .ui file combo box is updated!
 	availableTriggerIds = ['trig1', 'trig2', 'trig3', 'motion']
+	availableTrigger1Actions = ['none', 'record end', 'exposure', 'shutter gating', 'frame sync']
+	availableTrigger2Actions = ['none', 'record end', 'exposure', 'shutter gating', 'frame sync']
+	availableTrigger3Actions = ['none', 'record end']
+	#Analog triggers can take no action.
+	availableMotionTriggerActions = ['none', 'record end']
 	
 	
 	def __init__(self, window):
@@ -26,11 +53,11 @@ class Triggers(QtWidgets.QDialog):
 		
 		self.uiUnsavedChangesWarning.hide()
 		
+		self.trigger3VoltageTextTemplate = self.uiTrigger3ThresholdVoltage.text()
+		
 		
 		# Set up panel switching.
 		#DDR 2018-07-24 It's impossible to associate an identifier with anything in QT Designer. Painfully load the identifiers here. Also check everything because I will mess this up next time I add a trigger.
-		if(self.uiActiveTrigger.count() != len(self.availableTriggerIds)):
-			raise Exception("Trigger screen available trigger IDs does not match the number of textual entries in uiActiveTrigger.")
 		if(self.uiTriggerScreens.count() != len(self.availableTriggerIds)):
 			raise Exception("Trigger screen available trigger IDs does not match the number of uiTriggerScreens screens.")
 		
@@ -39,18 +66,127 @@ class Triggers(QtWidgets.QDialog):
 			print(f'{currentScreenId} is not a known trigger ID, defaulting to {self.availableTriggerIds[0]}')
 			currentScreenId = self.availableTriggerIds[0]
 		
-		currentScreenIndex = self.availableTriggerIds.index(currentScreenId)
-		self.uiActiveTrigger.setCurrentIndex(currentScreenIndex)
-		self.changeShownTrigger(currentScreenIndex)
+		self.changeShownTrigger(currentScreenId)
+		self.uiTrig1Tab.clicked.connect(lambda: self.changeShownTrigger('trig1'))
+		self.uiTrig2Tab.clicked.connect(lambda: self.changeShownTrigger('trig2'))
+		self.uiTrig3Tab.clicked.connect(lambda: self.changeShownTrigger('trig3'))
+		self.uiMotionTriggerTab.clicked.connect(lambda: self.changeShownTrigger('motion'))
 		
-		# Widget behavour.
-		self.uiActiveTrigger.currentIndexChanged.connect(self.changeShownTrigger)
 		
+		#Set up state init & events.
 		self.uiRecordMode.clicked.connect(lambda: window.show('record_mode'))
 		self.uiTriggerDelay.clicked.connect(lambda: window.show('trigger_delay'))
+		self.uiApply.clicked.connect(lambda: self and dbg())
+		#self.uiDebugC.clicked.connect(saveEverything()) TODO: This.
 		self.uiDone.clicked.connect(window.back)
+		
+		#OK, so, triggerCapabilities is a constant, that's good. Then
+		#we have triggerConfiguration, which is one variable. We'll
+		#watch that variable and just update everything when it changes
+		#for now. A better way to do it would be to only update state
+		#that changed since the last time it updated, so we don't wipe
+		#state from unchanged sub-fields. (Note: The reason this is one
+		#variable instead of many is that triggers are special, and
+		#they need to be updated somewhat atomically. So one variable,
+		#so you can't mess it up… I'm not sure this is the best design,
+		#but it's what we decided on back when. We do variable
+		#change aggregation for the sensor, we could do it here too.
+		#But perhaps forcing it safe is a good decision, if less
+		#convenient.) So, when *the* state updates, we'll just update
+		#everything and it should work for now.
+		
+		#This shows the three times of data we have. One updates only once, one updates whenever the data is changed, and the final updates every frame.
+		self.setCapabilities(api.get('triggerCapabilities'))
+		
+		api.observe('triggerConfiguration', self.updateTriggerConfiguration)
+		
+		self.triggerStateUpdateTimer = QtCore.QTimer()
+		self.triggerStateUpdateTimer.timeout.connect(self.updateTriggerState)
+		self.triggerStateUpdateTimer.start(1000/30) #Update at 30fps since we're somewhat cpu-bound on this task.
+		
+		
 	
 	
-	def changeShownTrigger(self, index):
-		self.uiTriggerScreens.setCurrentIndex(index)
-		settings.setValue('active trigger', self.availableTriggerIds[index])
+	def onShow(self):
+		#Don't poll the trigger states while hidden. But do show with accurate info when we start.
+		self.updateTriggerState()
+		self.triggerStateUpdateTimer.start()
+		
+	def onHide(self):
+		self.triggerStateUpdateTimer.stop()
+	
+	
+	def changeShownTrigger(self, triggerId):
+		tabs = [
+			(self.availableTriggerIds[0], self.uiTrig1Tab),
+			(self.availableTriggerIds[1], self.uiTrig2Tab),
+			(self.availableTriggerIds[2], self.uiTrig3Tab),
+			(self.availableTriggerIds[3], self.uiMotionTriggerTab) ]
+		for id, tab in tabs:
+			tab.keepActiveLook = id == triggerId
+			tab.refreshStyle()
+			
+		
+		self.uiTriggerScreens.setCurrentIndex(
+			self.availableTriggerIds.index(triggerId) )
+		
+		settings.setValue('active trigger', triggerId)
+	
+	
+	def setCapabilities(self, config):
+		"""Configure the UI with the capabilities reported by the API.
+		
+			Note: Most of the capabilities are hard-coded into the .ui
+			file right now. We only have one camera, so we only have
+			one set of capabilities, so it doesn't make a lot of sense
+			to pull this out right now.
+		"""
+		
+		self.uiTrigger1ThresholdVoltage.setMinimum(config["trig1"]["thresholdMin"])
+		self.uiTrigger1ThresholdVoltage.setMaximum(config["trig1"]["thresholdMax"])
+		self.uiTrigger2ThresholdVoltage.setMinimum(config["trig2"]["thresholdMin"])
+		self.uiTrigger2ThresholdVoltage.setMaximum(config["trig2"]["thresholdMax"])
+	
+	
+	@pyqtSlot('QVariantMap', name="updateTriggerConfiguration")
+	@silenceCallbacks(
+		'uiTrigger1Action', 'uiTrigger1ThresholdVoltage', 'uiTrigger11mAPullup', 'uiTrigger120mAPullup', 'uiTrigger1Invert', 'uiTrigger1Debounce',
+		'uiTrigger2Action', 'uiTrigger2ThresholdVoltage', 'uiTrigger2Invert', 'uiTrigger2Debounce', 'uiTrigger220mAPullup',
+		'uiTrigger3Action', 'uiTrigger3ThresholdVoltage', 'uiTrigger3Invert', 'uiTrigger3Debounce',
+		'uiMotionTriggerAction', 'uiMotionTriggerDebounce', 'uiMotionTriggerInvert',
+	)
+	def updateTriggerConfiguration(self, config):
+		"""Update the displayed trigger settings."""
+		
+		self.uiTrigger1Action.setCurrentIndex(
+			self.availableTrigger1Actions.index(config['trig1']['action']) )
+		self.uiTrigger1ThresholdVoltage.setValue(config['trig1']['threshold'])
+		self.uiTrigger11mAPullup.setChecked(config['trig1']['pullup1ma'])
+		self.uiTrigger120mAPullup.setChecked(config['trig1']['pullup20ma'])
+		self.uiTrigger1Invert.setChecked(config['trig1']['invert'])
+		self.uiTrigger1Debounce.setChecked(config['trig1']['debounce'])
+		
+		self.uiTrigger2Action.setCurrentIndex(
+			self.availableTrigger2Actions.index(config['trig2']['action']) )
+		self.uiTrigger2ThresholdVoltage.setValue(config['trig2']['threshold'])
+		self.uiTrigger2Invert.setChecked(config['trig2']['invert'])
+		self.uiTrigger2Debounce.setChecked(config['trig2']['debounce'])
+		self.uiTrigger220mAPullup.setChecked(config['trig2']['pullup20ma'])
+		
+		self.uiTrigger3Action.setCurrentIndex(
+			self.availableTrigger3Actions.index(config['trig3']['action']) )
+		self.uiTrigger3ThresholdVoltage.setText(
+			self.trigger3VoltageTextTemplate.format(config['trig3']['threshold']) )
+		self.uiTrigger3Invert.setChecked(config['trig3']['invert'])
+		self.uiTrigger3Debounce.setChecked(config['trig3']['debounce'])
+		
+		#Most motion trigger settings are displayed in the motion trigger configuration screen.
+		self.uiMotionTriggerAction.setCurrentIndex(
+			self.availableMotionTriggerActions.index(config['motion']['action']) )
+		self.uiMotionTriggerDebounce.setChecked(config['motion']['debounce'])
+		self.uiMotionTriggerInvert.setChecked(config['motion']['invert'])
+	
+	
+	def updateTriggerState(self):
+		state = api.get('triggerState')
+		#TODO: Update state here.
