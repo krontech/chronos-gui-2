ce4e6a167741ae241d66b1aef7bbef906bff5706
David Roberts <ddr@krontech.ca>
2018-12-11 15:40:20 -0800

    Added a standard subheader element and a comment element.
    	- The subheader is below a header in importance, but above a label.
    	- The comment element, like touch margins, does not show on camera.

---
+++ src/widgets/comment.py
@@ -0,0 +1,32 @@
+from PyQt5.QtWidgets import QLabel
+from PyQt5.QtCore import QSize, Qt
+
+from debugger import *; dbg
+
+
+class Comment(QLabel):
+	def __init__(self, parent=None, showHitRects=False):
+		super().__init__(parent)
+		
+		if not showHitRects:
+			self.hide()
+			return
+		
+		self.setStyleSheet(f"""
+			font-family: URW Chancery L; /*Karumbi was a nice font for this, but had really huge line-spacing. Not what we need here. Also, this font is not on the camera, but it doesn't matter because it'll never be displayed there.*/
+			font-size: 14px;
+			color: rgba(15,83,15,200); /*Slightly transparent dark green looks nice. 🙂*/
+		""" + self.styleSheet())
+		
+		# Set some default text, so we can see the widget.
+		if not self.text():
+			self.setText('~comment~')
+		
+		self.setWordWrap(True)
+		self.setAlignment(Qt.AlignTop | Qt.AlignLeft)
+
+
+	def sizeHint(self):
+		return QSize(241, 51)
+		
+		
---
+++ src/widgets/subheader_plugin.py
@@ -0,0 +1,45 @@
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtDesigner import QPyDesignerCustomWidgetPlugin
+from plugin_settings import showHitRects
+
+from subheader import Subheader
+
+
+class SubheaderPlugin(QPyDesignerCustomWidgetPlugin):
+
+	def __init__(self, parent=None):
+		super().__init__(parent)
+
+		self.initialized = False
+
+	def initialize(self, core):
+		if not self.initialized:
+			self.initialized = True
+
+	def isInitialized(self):
+		return self.initialized
+
+	def createWidget(self, parent):
+		return Subheader(parent, showHitRects=showHitRects)
+
+	def name(self):
+		return "Subheader"
+
+	def group(self):
+		return "Chronos"
+
+	def icon(self):
+		return QIcon(QPixmap("../../assets/qt_creator/subheader.svg"))
+
+	def toolTip(self):
+		return """A subheader - above label but below header
+in importance - in the Chronos style."""
+
+	def whatsThis(self):
+		return self.toolTip() # ¯\_(ツ)_/¯ I have no idea how to trigger this.
+
+	def isContainer(self):
+		return False
+
+	def includeFile(self):
+		return "subheader"
