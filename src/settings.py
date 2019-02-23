1812b31155bc2d326814818c62a58926ee5de039
David Roberts <ddr@krontech.ca>
2019-02-22 20:30:10 -0800

    Core libs: settings.value and settings.setValue now accept any, vs str.

--- src/settings.py
+++ src/settings.py
@@ -7,12 +7,12 @@ Observe a key with a callback, called when the key is changed or initialized.
 
 from collections import defaultdict
 from typing import Callable, Optional
+import json
 
 from PyQt5.QtCore import QSettings
 
-_settings = QSettings('Krontech', 'back-of-camera interface')
+_settings = QSettings('Krontech', 'back-of-camera interface') #in ~/.config/Krontech/back-of-camera interface.conf
 _callbacks = defaultdict(list)
-
 
 def observe(key: str, callback: Callable[[Optional[str]], None]) -> None:
 	"""Watch key for changes, calling callback when set. As with api.observe,
@@ -38,21 +38,19 @@ def observe(key: str, callback: Callable[[Optional[str]], None]) -> None:
 	_callbacks[key].append(callback)
 
 
-def setValue(key: str, value: str) -> str:
+def setValue(key: str, value: any) -> None:
 	"""See http://doc.qt.io/qt-5/qsettings.html#setValue"""
 	
 	#Do some typechecking, this gets confusing because the value is cast to a string (according to non-python rules) only after the app is restarted.
 	if not isinstance(key, str):
 		raise TypeError(f'settings.setValue(key, value) only accepts str keys, because that\'s what the underlying store accepts. It got passed the key {key}, a {type(key)}.')
-	if not isinstance(value, str):
-		raise TypeError(f'settings.setValue(key, value) only accepts str values, because that\'s what the underlying store accepts. It got passed the value {value}, a {type(value)}.')
 	
 	for callback in _callbacks[key]:
 		callback(value)
 	
-	_settings.setValue(key, value)
+	_settings.setValue(key, json.dumps(value))
 
 
-def value(key: str, default: str = '') -> str:
+def value(key: str, default: any = '') -> any:
 	"""See http://doc.qt.io/qt-5/qsettings.html#value"""
-	return _settings.value(key, default)
+	return json.loads(_settings.value(key, json.dumps(default)))
