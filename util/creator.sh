53dacc49e6e16f707f2b3b4daf9b3fc9ea54fe61
David Roberts <ddr@krontech.ca>
2019-08-23 16:24:08 -0700

    Simplification to creator.sh and designer.sh.
    	- For some reason, these aren't launching properly on my
    		system any more, before or after the refactor.

--- util/creator.sh
+++ util/creator.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-
-if [ -d src/widgets ]; then
-	cd src/screens #set default qt designer save location
-	PYQTDESIGNERPATH=$PYQTDESIGNERPATH:../widgets PYTHONPATH=$PYTHONPATH:../ qtcreator
-else 
-	if [ -d ../src/widgets ]; then
-		cd ../src/screens
-		PYQTDESIGNERPATH=$PYQTDESIGNERPATH:../widgets PYTHONPATH=$PYTHONPATH:../ qtcreator
-	else
-		xmessage -nearmouse "Error: Couldn't find ~chronos-gui-2/src/widgets/."
-	fi
-fi
+set -euxo pipefail
+cd "$(dirname "$0")/../src/screens"
+PYQTDESIGNERPATH="${PYQTDESIGNERPATH:-}:../widgets" PYTHONPATH="${PYTHONPATH:-}:../" qtcreator
--- util/designer.sh
+++ util/designer.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-
-if [ -d src/widgets ]; then
-	cd src/screens #set default qt designer save location
-	PYQTDESIGNERPATH=$PYQTDESIGNERPATH:../widgets PYTHONPATH=$PYTHONPATH:../ designer
-else 
-	if [ -d ../src/widgets ]; then
-		cd ../src/screens
-		PYQTDESIGNERPATH=$PYQTDESIGNERPATH:../widgets PYTHONPATH=$PYTHONPATH:../ designer
-	else
-		xmessage -nearmouse "Error: Couldn't find ~chronos-gui-2/src/widgets/."
-	fi
-fi
+set -euxo pipefail
+cd "$(dirname "$0")/../src/screens"
+PYQTDESIGNERPATH="${PYQTDESIGNERPATH:-}:../widgets" PYTHONPATH="${PYTHONPATH:-}:../" designer
