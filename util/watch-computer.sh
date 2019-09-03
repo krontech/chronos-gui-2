1ef20fa44931fba5caf6f09dd7efb87974be1456
David Roberts <ddr@krontech.ca>
2019-09-03 14:42:32 -0700

    Fixed bug in watch-computer.sh where it was including too much.
    	- This was caused by a missing \.
    		- This will not occur again because -e will let us know now.
    	- I now understand the whole script much better. Commented!
    		- The t in the output means time, and that's from --times.
    		  We could use --checksums if we wanted to.
    	- Expanded some parameter names.
    		- Single letter parameters are fine for typing, but for
    		  scripts it's much better to use the long forms.

--- util/watch-computer.sh
+++ util/watch-computer.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
-set -uo pipefail
+set -euo pipefail
 IFS=$'\n'
 
 trap "exit;" SIGINT SIGTERM #The following loop works fine in Konsole but won't ever stop on Gnome Terminal.
 
+cd "$(dirname "$0")/.." #Always sync project root directory.
+
 while true; do 
 	find -regex '\./[^_\.].*' ! -name git_description ! -path 'util/stats_reported' | entr -d bash -c "
-		if [[ \$(cat git_description) != \$(git describe --tags --always) ]]; then #Only update when description changes, results in some thrashing otherwise.
+		if [[ \$(cat git_description) != \$(git describe --tags --always) ]]; then #Only update when description changes, results in some thrashing otherwise when using --times.
 			git describe --tags --always > git_description
 		fi
-		rsync -ir ./ \"${CAM_ADDRESS:-root@192.168.12.1}:/root/gui/\" \
-			--delete --links \
-			--rsh=\"/usr/bin/sshpass -p $CHRONOS_PASSWORD ssh -l root\" --times --inplace \
-			--exclude \"__pycache__\"
+		
+		#Use --times instead of --checksum because it means we can touch a file (ie, save it) and have the camera script restart. Checksum is more proper, and may take longer to run, but I haven't noticed any difference in my testing. Note that with --times, we redeploy the whole project when we switch computers, because we ran 'git checkout' at different times.
+		rsync --recursive --delete --links --inplace --times --itemize-changes \
+			./ \"${CAM_ADDRESS:-root@192.168.12.1}:/root/gui/\" \
+			--rsh=\"/usr/bin/sshpass -p $CHRONOS_PASSWORD ssh -l root\" \
+			--exclude \"__pycache__\" \
 			--exclude \"/.git\" \
 			--exclude \".mypy_cache\" \
 			--exclude \"util/stats_reported\" \
-			--exclude \"src/read_jog_wheel_encoder\"
+			--exclude \"src/read_jog_wheel_encoder\" \
+			--exclude \".directory\" \
 	"
 done
