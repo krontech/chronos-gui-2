db764ab16db948a39eaa93c8d4148916352c5497
David Roberts <ddr@krontech.ca>
2019-09-27 22:18:12 -0700

    Main2: Made recording work again.

--- src/screens/main2.py
+++ src/screens/main2.py
@@ -13,6 +13,9 @@ import settings
 #import animate
 import api2 as api
 
+RECORDING_MODES = {'START/STOP':1, 'SOFT_TRIGGER':2, 'VIRTUAL_TRIGGER':3}
+RECORDING_MODE = RECORDING_MODES['START/STOP']
+
 
 class Main(QWidget):
 	def __init__(self, window):
@@ -24,6 +27,7 @@ class Main(QWidget):
 		self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
 		self.setAttribute(QtCore.Qt.WA_TranslucentBackground, True)
 		
+		self._window = window
 		
 		#############################
 		#   Button action binding   #
@@ -225,7 +229,10 @@ class Main(QWidget):
 		
 		
 		#Record / stop
+		self.uiRecordTemplate = self.uiRecord.text()
+		self.uiRecord.clicked.connect(self.toggleRecording)
 		
+		api.observe('state', self.onStateChange)
 		
 		#Play & save
 		
@@ -284,6 +291,7 @@ class Main(QWidget):
 		
 		def uiExternalMediaPaintEvent(evt, meterPaddingX=15, meterOffsetY=2, meterHeight=10):
 			"""Draw the disk usage bar on the external media button."""
+			type(self.uiExternalMedia).paintEvent(self.uiExternalMedia, evt) #Invoke the superclass to - hopefully - paint the rest of the button before we deface it with our square.
 			if externalMediaRatioFull == -1:
 				return
 			
@@ -389,3 +397,94 @@ class Main(QWidget):
 		self.uiBattery.setText(
 			self._batteryTemplate.format(
 				round(charge*100) ) )
+	
+	
+	def toggleRecording(self, *_):
+		"""Hard start or stop recording. Doesn't use trigger/io signals."""
+		
+		if RECORDING_MODE != RECORDING_MODES['START/STOP']:
+			return
+		
+		if api.apiValues.get('state') == 'idle':
+			self.startRecording()
+		else:
+			self.stopRecording()
+	
+	
+	#Invoked by hardware button, in ~/src/main.py.
+	def publicToggleRecordingState(self, *_):
+		"""Switch the camera between 'not recording' and 'recording'."""
+		
+		if RECORDING_MODE != RECORDING_MODES['START/STOP']:
+			return
+		
+		self.toggleRecording()
+		
+		#If we're not on this screen when the red record button is pressed, go there.
+		if self._window.currentScreen != 'main':
+			self._window.show('main')
+	
+	@staticmethod
+	def setVirtualTrigger(state: bool):
+		"""Set the virtual trigger signal high or low.
+			
+			May or may not start a recording, depending on how
+			trigger/io is set up.
+			
+			(See trigger/io screen for details.)"""
+		
+		#Can't use self.uiRecord.setText text here becasue we don't
+		#know what the virtual trigger is actually hooked up to, on
+		#what delay, so we have to wait for the signal. (We could,
+		#but writing that simulation would be a lot of work.)
+		
+		if RECORDING_MODE == RECORDING_MODES['START/STOP']:
+			pass #Taken care of by publicToggleRecordingState
+		
+		elif RECORDING_MODE == RECORDING_MODES['SOFT_TRIGGER']:
+			raise ValueError('Soft trigger not available in FPGA.')
+		
+		elif RECORDING_MODE == RECORDING_MODES['VIRTUAL_TRIGGER']:
+			virtuals = settings.value('virtually triggered actions', {})
+			if virtuals:
+				api.setSync('ioMapping', dump('new io mapping', {
+					action: { 
+						'source': 'alwaysHigh' if state else 'none',
+						'invert': config['invert'],
+						'debounce': config['debounce'],
+					} for action, config in virtuals.items()
+				}))
+			else:
+				#TODO: Log warning visually for operator, not just to console.
+				log.warn('No virtual triggers configured!')
+		
+		else:
+			raise ValueError(F'Unknown RECORDING_MODE: {RECORDING_MODE}.')
+	
+	
+	def publicStartVirtualTrigger(self, *_):
+		self.setVirtualTrigger(True)
+	
+	def publicStopVirtualTrigger(self, *_):
+		self.setVirtualTrigger(False)
+	
+	
+	def onStateChange(self, state):
+		#This text update takes a little while to fire, so we do it in the start and stop recording functions as well so it's more responsive when the operator clicks.
+		self.uiRecord.setText(
+			self.uiRecordTemplate.format(
+				state = 'Record' if state == 'idle' else 'Stop    ' ) )
+		
+		if state == 'idle' and settings.value('autoSaveVideo', False): #[autosave]
+			self._window.show('play_and_save')
+	
+	
+	def startRecording(self):
+		self.uiRecord.setText(
+			self.uiRecordTemplate.format(state='Stop    ') ) #Show feedback quick, the signal takes a noticable amount of time.
+		api.control.callSync('startRecording')
+	
+	def stopRecording(self):
+		self.uiRecord.setText(
+			self.uiRecordTemplate.format(state='Record') )
+		api.control.callSync('stopRecording')
