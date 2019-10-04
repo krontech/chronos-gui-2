1c0b0274653eefc3a33f7c3a3aa5dbc65e37ab26
David Roberts <ddr@krontech.ca>
2019-10-03 20:38:55 -0700

    Added graphs for startup times.

--- util/stats.node.js
+++ util/stats.node.js
@@ -1,27 +1,42 @@
 /* jshint globalstrict:true, esnext:true, node:true */
 "use strict";
 
-//run server with nodejs --harmony ddrop.js
-//or with sudo systemctl restart ddrop (if you've installed ddrop.service)
-//logs via stdout or journalctl -u ddrop -f
+//run server with:
+//	nohup node stats.node.js & disown
 
-const addr='192.168.1.174', port = 19861;
+const addr='192.168.1.55', port = 19861;
 const output_folder='stats_reported';
 
 const fs = require('fs');
 const http = require('http');
 
+console.log("Running stats server at http://"+addr+":"+port+"/");
+
 if (!fs.existsSync(output_folder)){
+	console.log(`Making output folder at ${output_folder}.`)
 	fs.mkdirSync(output_folder);
 }
 
-// Configure our HTTP server to respond with Hello World to all requests.
+//after setup, don't die on errors
+process.on('uncaughtException', function (err) {
+	console.error(err.stack);
+});
+
+
 const server = http.createServer(function (req, res) {
 	console.info('' + Date() + ': handling ' + req.method + ' from ' + req.headers.host);
 	
-	if(req.method === "POST") {
+	if(req.method === 'POST' && req.url === '/') {
 		let data = '';
-		req.on('data', function(dat) { data += dat; }); //implement limits here if dos issues
+		req.on('data', function(dat) {
+			data += dat;
+			if(data.length > 2000) { 
+				console.error('Request too long.')
+				req.abort()
+				res.writeHead(403, {"Content-Type": "text"});
+				res.end('{"error": "#KIL8cK"}', 'utf8'); //Request too long.
+			}
+		});
 		req.on('end', function() {
 			console.log('got', data)
 			let fields = JSON.parse(data);
@@ -33,18 +48,29 @@ const server = http.createServer(function (req, res) {
 			res.writeHead(200, {"Content-Type": "text"});
 			res.end('ok');
 		});
-	} else {
-		interfaceHTML = fs.readFileSync('stats.html', 'utf8');
+	} 
+	
+	else if(req.method === 'GET' && req.url === '/') {
+		const interfaceHTML = fs.readFileSync('stats.html', 'utf8');
 		res.writeHead(200, {"Content-Type": "text/html"});
-		res.end(e, interfaceHTML);
+		res.end(interfaceHTML, 'utf8');
+	} 
+	
+	else if(req.method === 'GET' && req.url === '/start_up_time.jsonl') {
+		const data = fs.readFileSync(`${output_folder}/start_up_time.jsonl`, 'utf8');
+		res.writeHead(200, {"Content-Type": "text/jsonl"});
+		res.end(data, 'utf8');
+	} 
+	
+	else if(req.method === 'GET' && req.url === '/screen_cache_time.jsonl') {
+		const data = fs.readFileSync(`${output_folder}/screen_cache_time.jsonl`, 'utf8');
+		res.writeHead(200, {"Content-Type": "text/jsonl"});
+		res.end(data, 'utf8');
+	} 
+	
+	else {
+		res.writeHead(404, {"Content-Type": "text/jsonl"});
+		res.end('{"error": "404 not found"}', 'utf8');
 	}
-});
-
-//listen for incoming connections
+})
 server.listen(port, addr);
-console.log("Server running at http://"+addr+":"+port+"/");
-
-//don't die on errors
-process.on('uncaughtException', function (err) {
-	console.error(err.stack);
-});
