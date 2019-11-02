krontech/chronos-gui-2

commit e48aa90745a9de103d9d1cb9415b5fbd8397189d

Double click to zoom works now. Next up, pinch.

src/main.py
@@ -497,6 +497,7 @@ def connectHardwareEvents(app, hardware):
 
 if __name__ == '__main__':
 	app = QtWidgets.QApplication(sys.argv)
+	app.setDoubleClickInterval(5) #400 is default and hard to do with fingers. Also this doesn't work. "The default value on X11 is 400 milliseconds", but how do you change that? Since we only use it one place, we'll just ignore it and debounce our own.
 	
 	font = QtGui.QFont("Roboto", 12)
 	font.setLetterSpacing(QtGui.QFont.AbsoluteSpacing, 0.25)

src/api2.py
@@ -607,6 +607,9 @@ _camState['error'] = '' #Last error is reported inline sometimes.
 if 'videoSegments' not in _camState:
 	log.warn('videoSegments not found in availableKeys (pychronos/issues/31)')
 	_camState['videoSegments'] = []
+if 'videoZoom' not in _camState:
+	log.warn('videoZoom not found in availableKeys (pychronos/issues/52)')
+	_camState['videoZoom'] = 1
 _camStateAge = {k:0 for k,v in _camState.items()}
 
 class APIValues(QObject):

src/widgets/interactive_video_area.py
@@ -1,7 +1,10 @@
 # -*- coding: future_fstrings -*-
 
-from PyQt5.QtWidgets import QWidget
-from PyQt5.QtCore import pyqtProperty, QSize
+from time import time
+
+from PyQt5.QtWidgets import QWidget, QLabel
+from PyQt5.QtCore import QSize, QEvent, Qt
+import PyQt5.QtGui as QtGui
 
 from debugger import *; dbg
 from animate import delay
@@ -12,9 +15,13 @@ except Exception:
 	log.warn('Unable to import api, DirectAPILinkPlugin disabled. (Some widgets will not have any effect when used.)')
 	api = None
 
+
+
 class InteractiveVideoArea(QWidget):
 	def sizeHint(self):
 		return QSize(141, 141)
+	
+	zoomLabelTemplate = "🔎 {zoom:0.1f}x Zoom"
 
 	def __init__(self, parent=None, showHitRects=False):
 		super().__init__(parent)
@@ -25,13 +32,78 @@ class InteractiveVideoArea(QWidget):
 				border: 4px solid black;
 			""")
 		
+		
+		self.zoomLabel = QLabel(self)
+		self.zoomLabel.setStyleSheet(f"""
+			color: white;
+			background-color: rgba(0,0,0,128);
+		""")
+		self.zoomLabel.setText(self.zoomLabelTemplate.format(zoom=2))
+		self.zoomLabel.show()
+		
+		
 		if showHitRects: #Make black background show up in Designer. Must be async for some reason.
 			delay(self, 0, lambda: self.setAutoFillBackground(True))
+		
+		if api:
+			self.lastClickTime = 0
+			api.observe('videoZoom', self.updateVideoZoom)
+	
 	
 	def showEvent(self, evt):
-		api and api.video.call('configure', {
+		log.print('SHOW EVENT')
+		api.video.call('set', {'videoZoom': 1})
+		api.video.call('configure', {
 			'xoff': max(0, min(self.x(), 800-self.width())),
 			'yoff': max(0, min(self.y(), 480-self.height())),
 			'hres': max(200, min(self.width(), 800)),
 			'vres': max(200, min(self.height(), 480)),
 		})
+		
+		
+	if api:
+		def event(self, evt: QEvent):
+			#add zoom here
+			
+			return super().event(evt)
+		
+		
+		def mousePressEvent(self, evt):
+			clickTimeDelta = time() - self.lastClickTime
+			self.lastClickTime = time()
+			if clickTimeDelta < 0.5:
+				self.lastClickTime = 0
+				self.doubleClickHandler(evt)
+		
+		#Don't use this, because the timeout is too low for fingers. Set in x11 somewhere, writing mousePressEvent was easier that changing it (and deploying those changes reliably).
+		#def mouseDoubleClickEvent(self, evt):
+		#	print('double click!')
+		
+		def doubleClickHandler(self, evt): #This never gets called by Qt, so we have to call it ourselves.
+			"""When double-clicked, set the zoom level to the next."""
+			zoomLevels = sorted([1, 4, self.oneToOneZoomLevel()])
+			zoom = api.apiValues.get('videoZoom')
+			closestZoom = sorted(zoomLevels, key=lambda zl:abs(zl-zoom))[0]
+			nextZoomLevel = zoomLevels[(zoomLevels.index(closestZoom)+1) % len(zoomLevels)]
+			api.video.call('set', {'videoZoom': nextZoomLevel})
+		
+		
+		def oneToOneZoomLevel(self):
+			"""Return the "natural" zoom of the video.
+				
+				This is the zoom level at which the recorded pixels have a 1:1
+				correspondance with those on the screen."""
+				
+			res = api.apiValues.get('resolution')
+			return min(
+				res['hRes'] / self.width(),
+				res['vRes'] / self.height(),
+			)
+		
+		def realZoomLevel(self):
+			return api.apiValues.get('videoZoom') / self.oneToOneZoomLevel()
+		
+		def updateVideoZoom(self, level):
+			self.zoomLabel.setText(
+				self.zoomLabelTemplate.format(
+					zoom=self.realZoomLevel() ) )

commit a1f497d8b8d5a07ecd92b496b3f9c54e8b0d0414

Fixed regression where drop-down menus wouldn't stay open.
	- Bug introduced in e48aa90.

src/main.py
@@ -503,7 +503,7 @@ def connectHardwareEvents(app, hardware):
 
 if __name__ == '__main__':
 	app = QtWidgets.QApplication(sys.argv)
-	app.setDoubleClickInterval(5) #400 is default and hard to do with fingers. Also this doesn't work. "The default value on X11 is 400 milliseconds", but how do you change that? Since we only use it one place, we'll just ignore it and debounce our own.
+	app.setDoubleClickInterval(500) #400 is default and hard to do with fingers. Also this doesn't work. "The default value on X11 is 400 milliseconds", but how do you change that? Since we only use it one place, we'll just ignore it and debounce our own.
 	
 	font = QtGui.QFont("Roboto", 12)
 	font.setLetterSpacing(QtGui.QFont.AbsoluteSpacing, 0.25)