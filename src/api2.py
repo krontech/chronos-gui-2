10b394b3b5575f8397e6279014a9db8f2f9ab1f2
David Roberts <robertsdavidddr@gmail.com>
2019-11-05 20:26:44 -0800

    Added start of api2.networkInterfaces. Currently loads but does not update.

--- src/api2.py
+++ src/api2.py
@@ -966,6 +966,270 @@ externalPartitions = ExternalPartitions()
 del ExternalPartitions
 
 
+
+class NetworkInterfaces(QObject):
+	def __init__(self):
+		"""
+			NetworkInterfaces is a list of the plugged-in network connections.
+			
+			```python
+				[{
+					"path": b"/org/freedesktop/NetworkManager/Devices/1"
+					"name": "Ethernet",
+					"ipAddr": "192.168.100.166" or "2001:0db8:85a3::8a2e:0370:7334"
+				}, {
+					...
+				}]
+			```
+			
+			You can `networkInterfaces.observe(callback)` to get updates.
+			
+		"""
+		super().__init__()
+		self._connections = []
+		
+		#observers collection
+		self._callbacks = []
+		self.networkManager = QDBusInterface(
+			f"org.freedesktop.NetworkManager", #Service
+			f"/org/freedesktop/NetworkManager", #Path
+			f"org.freedesktop.NetworkManager", #Interface
+			QDBusConnection.systemBus(),
+		)
+		self.networkManager.setTimeout(10) #Set to 1000 after startup period.
+		
+		#Retry. This doesn't connect the first time, no matter what the time limit is. I don't know why, probably something in the start-on-demand logic.
+		if not self.networkManager.isValid():
+			self.networkManager = QDBusInterface(
+				f"org.freedesktop.NetworkManager", #Service
+				f"/org/freedesktop/NetworkManager", #Path
+				f"org.freedesktop.NetworkManager", #Interface
+				QDBusConnection.systemBus(),
+			)
+			self.networkManager.setTimeout(10)
+			
+			if not self.networkManager.isValid():
+				log.critical(f"Error: Can not connect to NetworkManager at {self.networkManager.service()}. ({self.networkManager.lastError().name()}: {self.networkManager.lastError().message()}) Try running `apt install udisks2`?")
+				raise Exception("D-Bus Setup Error")
+		
+		self.networkManager.setTimeout(1000)
+		
+		
+		#The .connect call freezes if we don't do this, or if we do this twice.
+		#This bug was fixed by Qt 5.11.
+		QDBusConnection.systemBus().registerObject(
+			f"/org/freedesktop/NetworkManager", 
+			self,
+		)
+		
+		self._acquireInterfacesCall = QDBusPendingCallWatcher(
+			self.networkManager.asyncCall('GetDevices')
+		)
+		self._acquireInterfacesCall.finished.connect(self._acquireInterfaceData)
+	def _acquireInterfaceData(self, reply):
+		reply = QDBusPendingReply(reply)
+		if reply.isError():
+			raise DBusException("%s: %s" % (reply.error().name(), reply.error().message()))
+		reply = reply.value()
+		
+		#[DDR 2019-11-05] Note: In Qt 5.7, we can't just go
+		#self._networkInterfaces['Device'].property(), like we could with
+		#self._networkInterfaces['Device'].call(), because .property()
+		#doesn't seem to work. afaik, it _should_ work, and the example in
+		#https://stackoverflow.com/questions/20042995/error-getting-dbus-interface-property-with-qdbusinterface
+		#which is directly relevant to our situation shows it working. Yet,
+		#I cannot figure out how to port it to Python. So we do it manually
+		#with the `'* property interface'`s.
+		
+		#Oh, hunh, https://doc.qt.io/archives/qt-5.7/qnetworkinterface.html
+		#is a thing. Nice to find that right as I finish this… >_<
+		
+		
+		self._networkInterfaces = [{
+			'Device': QDBusInterface( #Provides node.
+				f"org.freedesktop.NetworkManager", #Service
+				devicePath, #Path
+				f"org.freedesktop.NetworkManager.Device", #Interface
+				QDBusConnection.systemBus(),
+			),
+			'property interface': QDBusInterface( #Provides interface to get properties of previous node, because `.property()` is broken.
+				"org.freedesktop.NetworkManager", #Service
+				devicePath, #Path
+				"org.freedesktop.DBus.Properties",#Interface
+				QDBusConnection.systemBus()
+			),
+			'Device.Wired': QDBusInterface( #Provides node.
+				f"org.freedesktop.NetworkManager", #Service
+				devicePath, #Path
+				f"org.freedesktop.NetworkManager.Device.Wired", #Interface
+				QDBusConnection.systemBus(),
+			),
+		} for devicePath in reply ]
+		
+		for interfaces in self._networkInterfaces:
+			for networkInterface in interfaces.values():
+				networkInterface.setTimeout(1000)
+				if not networkInterface.isValid():
+					log.critical(f"Error: Can not connect to udisks2 at {networkInterface.service()}. ({networkInterface.lastError().name()}: {networkInterface.lastError().message()}) Try running `apt install udisks2`?")
+					raise Exception("D-Bus Setup Error")
+			
+			#Deadlock fix as above.
+			QDBusConnection.systemBus().registerObject(
+				interfaces['Device'].path(), self )
+		
+		print('ok → ', self.activeConnections())
+		#dbg()
+	
+	#	QDBusConnection.systemBus().connect(
+	#		f"org.freedesktop.UDisks2", #Service
+	#		f"/org/freedesktop/UDisks2", #Path
+	#		f"org.freedesktop.DBus.ObjectManager", #Interface
+	#		'InterfacesAdded', #Signal
+	#		self.__interfacesAddedEvent,
+	#	)
+	#	
+	#	QDBusConnection.systemBus().connect(
+	#		f"org.freedesktop.UDisks2", #Service
+	#		f"/org/freedesktop/UDisks2", #Path
+	#		f"org.freedesktop.DBus.ObjectManager", #Interface
+	#		'InterfacesRemoved', #Signal
+	#		self.__interfacesRemovedEvent,
+	#	)	
+	#	
+	#	for name, data in QDBusReply(self.uDisks2ObjectManager.call('GetManagedObjects')).value().items():
+	#		self.__interfacesAdded(name, data)
+	#
+	def __getitem__(self, i):
+		return self._connections[i]
+	
+	def __repr__(self):
+		#pdb uses repr instad of str (which imo is more appropriate for an interactive debugging session)
+		return f'{type(self)} ({self._connections})'
+	
+	def activeConnections(self):
+		connections = []
+		for interfaces in self._networkInterfaces:
+			carrier = QDBusReply(
+				interfaces['property interface'].call('Get',
+					'org.freedesktop.NetworkManager.Device.Wired', 'Carrier' ) ) #Interface, Property
+			if carrier.isValid() and carrier.value():
+				connections.append({
+					'name': QDBusReply(
+						interfaces['property interface'].call('Get',
+							'org.freedesktop.NetworkManager.Device', 'Interface' ) ).value() #Interface, Property
+				})
+		return connections
+		
+	
+	def observe(self, callback):
+		"""Add a function to get called when a volume is mounted or unmounted.
+			
+			The added function is immediately invoked."""
+		
+		assert callable(callback), f"Callback is not callable. (Expected function, got {callback}.)"
+		self._callbacks += [callback]
+		callback(self._connections)
+	
+	def unobserve(self, callback):
+		"""Stop a function from getting called when a volume is mounted or unmounted."""
+		
+		assert callable(callback), f"Callback is not callable. (Expected function, got {callback}.)"
+		self._callbacks = list(filter(
+			lambda existingCallback: existingCallback != callback, 
+			self._callbacks ) )
+	
+	
+	
+	#@pyqtSlot('QDBusMessage')
+	#def __interfacesAddedEvent(self, msg):
+	#	self.__interfacesAdded(*msg.arguments())
+	#
+	#def __interfacesAdded(self, name, data):
+	#	if 'org.freedesktop.UDisks2.Filesystem' in data:
+	#		#"Now, for each file system which just got mounted, …"
+	#		
+	#		#Filter root, which is mounted on / and /media/mmcblk0p2.
+	#		if len(data['org.freedesktop.UDisks2.Filesystem']['MountPoints']) != 1:
+	#			return
+	#		
+	#		#Filter out whatever gets mounted to /boot.
+	#		if not bytes(data['org.freedesktop.UDisks2.Filesystem']['MountPoints'][0]).startswith(b'/media/'):
+	#			return
+	#		
+	#		log.debug(f"Partition mounted at {bytes(data['org.freedesktop.UDisks2.Filesystem']['MountPoints'][0]).decode('utf-8')}.") #toStdString() doesn't seem to exist, perhaps because we don't have std strings.
+	#		
+	#		self._connections += [{
+	#			'name': data['org.freedesktop.UDisks2.Block']['IdLabel'],
+	#			'device': name,
+	#			'uuid': data['org.freedesktop.UDisks2.Block']['IdUUID'], #Found at `/dev/disk/by-uuid/`.
+	#			'path': bytes(data['org.freedesktop.UDisks2.Filesystem']['MountPoints'][0])[:-1], #Trim off a null byte at the end, we don't need it in python.
+	#			'size': data['org.freedesktop.UDisks2.Block']['Size'], #number of bytes, 64-bit positive integer
+	#			'readOnly': data['org.freedesktop.UDisks2.Block']['ReadOnly'],
+	#			'interface': 'usb' if True in [b'usb' in symlink for symlink in data['org.freedesktop.UDisks2.Block']['Symlinks']] else 'other', #This data comes in one message earlier, but it would be enough complexity to link the two that it makes more sense to just string match here.
+	#		}]
+	#		for callback in self._callbacks:
+	#			callback(self._connections)
+	#
+	#
+	#@pyqtSlot('QDBusMessage')
+	#def __interfacesRemovedEvent(self, msg):
+	#	self.__interfacesRemoved(*msg.arguments())
+	#
+	#def __interfacesRemoved(self, name, data):
+	#	if 'org.freedesktop.UDisks2.Partition' == data[0]:
+	#		#"Now, for each file system which just got removed, …"
+	#		self._connections = list(filter(
+	#			lambda partition: partition["device"] != name, 
+	#			self._connections ) )
+	#		for callback in self._callbacks:
+	#			callback(self._connections)
+	#
+	#
+	#def list(self):
+	#	return self._connections
+	#
+	#def usageFor(self, device: str, callback: Callable[[], Dict[str,int]]):
+	#	for partition in self._connections:
+	#		if partition['device'] == device:
+	#			df = subprocess.Popen(
+	#				['df', partition['path'], '--output=avail,used'], #used+avail != 1k-blocks
+	#				stdout=subprocess.PIPE,
+	#				stderr=subprocess.DEVNULL )
+	#			
+	#			def checkDf(*, timeout):
+	#				exitStatus = df.poll()
+	#				if exitStatus is None: #Still running, check again later.
+	#					#Standard clamped exponential decay. Keeps polling to a reasonable amount, frequent at first then low.
+	#					delay(self, timeout, lambda:
+	#						checkDf(timeout=max(1, timeout*2)) )
+	#				elif exitStatus: #df failure, raise an error
+	#					if exitStatus == 1:
+	#						#When a storage device with multiple partitions is removed,
+	#						#the observer fires once for each partition. This means
+	#						#that, for one partition, the client will issue a spurious
+	#						#call to this function with the stale partition's device.
+	#						log.debug(f'Unknown device {device}.')
+	#						log.debug(f'Known devices are {[p["device"] for p in self._connections]}.')
+	#					else:
+	#						raise Exception(f'df exited with error {exitStatus}')
+	#				else:
+	#					info = ( #Chop up df command output.
+	#						df.communicate()[0]
+	#						.split(b'\n')[1] #Remove first line, column headings
+	#						.split() #Each output is now in a list.
+	#					)
+	#					callback({
+	#						'available': int(info[0]),
+	#						'used': int(info[1]),
+	#						'total': int(info[0]) + int(info[1]),
+	#					})
+	#			delay(self, 0.20, lambda: #Initial delay, df usually runs in .17s.
+	#				checkDf(timeout=0.05) )
+networkInterfaces = NetworkInterfaces()
+del NetworkInterfaces
+
+
+
 #Perform self-test if launched as a standalone.
 if __name__ == '__main__':
 	from PyQt5.QtCore import QCoreApplication
