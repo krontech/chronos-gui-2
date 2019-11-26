95f7eab4311f43ccd573ced0a49d3a2bf6777d2d
David Roberts <ddr@krontech.ca>
2019-11-25 16:48:49 -0800

    Remote access screen now works, aside from a few glitches.

--- src/screens/remote_access.py
+++ src/screens/remote_access.py
@@ -1,13 +1,31 @@
 # -*- coding: future_fstrings -*-
 
 from random import shuffle, randint
+from hashlib import sha256
+import binascii
+import json
+import subprocess
 
 from PyQt5 import uic, QtWidgets, QtCore
+from PyQt5.QtGui import QPixmap
 
 from debugger import *; dbg
 
 import api2 as api
 import settings
+import external_process
+
+
+webServer = QtCore.QSettings('Krontech', 'web interface')
+
+
+def hexHash(*, password: str):
+	return binascii.hexlify(
+		sha256(
+			bytes(api.apiValues.get('cameraSerial'), 'utf-8') + 
+			sha256(bytes(password, 'utf-8')).digest()
+		).digest()
+	).decode('utf-8')
 
 
 letters = list('123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ!@#$%&')
@@ -26,6 +44,16 @@ def randomCharacters(count: int):
 	#Perhaps a random phrase would be better? If we install a dictionary...
 	#bash: shuf -n3 /usr/share/dict/american-english
 
+
+#from https://stackoverflow.com/questions/13179126/how-to-change-a-linux-user-password-from-python import setPassword
+def execSetPassword(userName:str, password:str):
+	p = subprocess.Popen(['chpasswd'], universal_newlines=True, shell=False, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	(stdout, stderr) = p.communicate(userName + ":" + password + "\n")
+	assert p.wait() == 0
+	if stdout or stderr:
+		raise Exception("Error encountered changing the password!")
+
+
 class RemoteAccess(QtWidgets.QWidget):
 	def __init__(self, window):
 		super().__init__()
@@ -39,82 +67,72 @@ class RemoteAccess(QtWidgets.QWidget):
 		# Widget binding.
 		self.uiDebug.clicked.connect(lambda: self and window and dbg())
 		
-		#Password-related stuff.
+		# Password-related stuff.
 		self.checkPasswordExists()
+		self.uiPassword.selectionChanged.connect(self.clearPasswordIfInitial)
 		self.uiPassword.editingFinished.connect(self.checkPasswordExists)
-		self.showPassword(settings.value('show network password', 'false') == 'true')
+		self.uiPassword.editingFinished.connect(self.onNewPassword)
 		self.uiShowPassword.clicked.connect(lambda: self.showPassword())
 		self.uiRandomisePassword.clicked.connect(self.randomisePassword)
 		
+		# Services
 		self.updateNetworkInterfaceInformation()
+		
+		#Only set status on startup, because we'll set it async as we start and stop services later.
+		self.uiHTTPStatus.showMessage(
+			f"Status: {'Running' if settings.value('http enabled', True) else 'Stopped'}", 
+			timeout = 0 )
+		
+		self.uiSSHStatus.showMessage(
+			f"Status: {'Running' if settings.value('ssh enabled', True) else 'Stopped'}", 
+			timeout = 0 )
+		
+		settings.observe('http enabled', True, lambda enabled: (
+			self.uiEnableHTTP.setChecked(enabled) ))
 		self.uiEnableHTTP.stateChanged.connect(self.updateNetworkInterfaceInformation)
+		self.uiEnableHTTP.stateChanged.connect(lambda state: self.setHTTP(on=bool(state)))
+		
+		settings.observe('ssh enabled', False, lambda enabled:
+			self.uiEnableSSH.setChecked(enabled) )
 		self.uiEnableSSH.stateChanged.connect(self.updateNetworkInterfaceInformation)
+		self.uiEnableSSH.stateChanged.connect(lambda state: self.setSSH(on=bool(state)))
+		
+		settings.observe('http port', 80, lambda enabled:
+			self.uiHTTPPort.setValue(enabled) )
 		self.uiHTTPPort.valueChanged.connect(self.updateNetworkInterfaceInformation)
+		self.uiHTTPPort.valueChanged.connect(lambda num: self.setHTTPPort(num))
+		
 		self.uiRandomiseHTTPPort.clicked.connect(lambda:
 			self.uiHTTPPort.setValue(randint(49152, 65535)) )
+		
+		settings.observe('ssh port', 22, lambda enabled:
+			self.uiSSHPort.setValue(enabled) )
 		self.uiSSHPort.valueChanged.connect(self.updateNetworkInterfaceInformation)
+		self.uiSSHPort.valueChanged.connect(lambda num: self.setSSHPort(num))
+		
 		self.uiRandomiseSSHPort.clicked.connect(lambda:
 			self.uiSSHPort.setValue(randint(49152, 65535)) )
 		
-		#api.observe('networkInterfaces', self.updateNetworkInterfaces)
-		self.uiNetworkInterface.currentIndexChanged.connect(self.showNetworkInterface)
+		api.networkInterfaces.observe(self.updateNetworkInterfaces)
+		self.uiNetworkInterface.currentIndexChanged.connect(self.updateNetworkInterface)
 		
+		# Navigation
 		self.uiDone.clicked.connect(window.back)
-		
-		"""
-			d g n interfaces
-			- gsn HTTP Access (None, Local, WWW)
-			- gsn SSH  Access (None, Local, WWW)
-			- gsn HTTP Port
-			- gsn SSH  Port
-			d g n lan IPv4 or v6 address - 192.168.1.79 (not .65?) or fe80::76e1:82ff:fe40:336e%%enp0s25
-			d g n www IPv4 or v6 address - 2001:569:7140:f500:11cf:9603:70a7:8af4
-			- gsn Web App password
-			- gsn SSH password
-			- - = not found, d = dbus, s = shell script, gsn = get/set/notify required
-			
-			docs: https://developer.gnome.org/NetworkManager/0.9/spec.html
-			org.freedesktop.NetworkManager.Device.Wired has ipv4/6 properties
-			gdbus introspect --system --dest org.freedesktop.NetworkManager.Device.Wired --object-path /org/freedesktop/NetworkManager/Device/Wired
-			
-			- Get network interfaces:
-				> gdbus introspect --system --dest org.freedesktop.NetworkManager --object-path /org/freedesktop/NetworkManager
-					- Has ActivateConnection method in org.freedesktop.NetworkManager
-					- Has GetDevices method in org.freedesktop.NetworkManager
-						> gdbus call --system --dest org.freedesktop.NetworkManager --object-path /org/freedesktop/NetworkManager --method org.freedesktop.NetworkManager.GetDevices
-							- ([objectpath '/org/freedesktop/NetworkManager/Devices/0', '/org/freedesktop/NetworkManager/Devices/1', '/org/freedesktop/NetworkManager/Devices/2', '/org/freedesktop/NetworkManager/Devices/3'],)
-						> gdbus introspect --system --dest org.freedesktop.NetworkManager --object-path /org/freedesktop/NetworkManager/Devices/0
-							- This is apparently a network connection - in this case, loopback.
-							- Links to IPv4/6 config.
-						> gdbus introspect --system --dest org.freedesktop.NetworkManager --object-path /org/freedesktop/NetworkManager/Devices/1
-							- eth0
-							- is plugged in?
-								- org.freedesktop.NetworkManager.Device.Wired property Carrier
-								- [implements g interfaces] Filter org.freedesktop.NetworkManager.GetDevices to get the list of plugged-in interfaces.
-							> gdbus introspect --system --dest org.freedesktop.NetworkManager --object-path /org/freedesktop/NetworkManager/DHCP4Config/0
-								- yields org.freedesktop.NetworkManager.DHCP4Config
-								- from ip_address' Dhcp4Config property
-								- [implements g n lan IPv4 or v6] in properties & PropertiesChanged signal.
-							> gdbus introspect --system --dest org.freedesktop.NetworkManager --object-path /org/freedesktop/NetworkManager/IP6Config/2
-								- yields org.freedesktop.NetworkManager.IP6Config
-								- from ip_address' Ip6Config property
-								- [implements g n g n www IPv4 or v6] in Addresses (first item) & PropertiesChanged signal.
-							- has Disconnect method
-								- https://developer.gnome.org/NetworkManager/0.9/spec.html#org.freedesktop.NetworkManager.Device.Disconnect
-						> gdbus introspect --system --dest org.freedesktop.NetworkManager --object-path /org/freedesktop/NetworkManager/Devices/2
-							- eth1
-						> gdbus introspect --system --dest org.freedesktop.NetworkManager --object-path /org/freedesktop/NetworkManager/Devices/3
-							- usb0
-		"""
 	
 	
 	def checkPasswordExists(self):
-		if self.uiPassword.text():
+		if self.uiPassword.text() == '«pass»' and webServer.value('password', '') == '':
+			self.showPassword(visible=True)
+		
+		if self.uiPassword.text() and (self.uiPassword.text() != '«pass»' or webServer.value('password', '')):
 			self.uiPasswordExplanation.hide()
 			self.uiAllConfigOptions.show()
 		else:
 			self.uiAllConfigOptions.hide()
 			self.uiPasswordExplanation.show()
+			
+	def onNewPassword(self):
+		self.setPassword(self.uiPassword.text())
 	
 	
 	def showPassword(self, visible=None):
@@ -128,7 +146,15 @@ class RemoteAccess(QtWidgets.QWidget):
 			raise ValueError(f'Unknown visibility value "{visible}".')
 		
 		self.uiPassword.setEchoMode(echoMode)
-		settings.setValue('show network password', 'true' if echoMode == QtWidgets.QLineEdit.Normal else "false")
+		
+		if echoMode == QtWidgets.QLineEdit.Normal and self.uiPassword.text() == "«pass»": #This value can not be entered; it's used as a flag to mark that we haven't fiddled with the password yet.
+			self.uiPassword.clear()
+	
+	
+	def clearPasswordIfInitial(self, *_):
+		if self.uiPassword.text() == "«pass»":
+			self.uiPassword.clear()
+			self.uiPassword.setEchoMode(QtWidgets.QLineEdit.Normal)
 	
 	
 	def randomisePassword(self):
@@ -139,43 +165,299 @@ class RemoteAccess(QtWidgets.QWidget):
 		self.checkPasswordExists() #Show controls. The editingFinished signal doesn't fire if we just change the contents like this, and textChanged fires too often (on every keystroke).
 	
 	
-	def updateNetworkInterfaceInformation(self, *_):
-		self.showNetworkInterface(self.uiNetworkInterface.currentIndex())
-	
-	
 	def updateNetworkInterfaces(self, interfaces):
-		if interfaces:
+		#Interfaces is like:
+		#	[{'address': IPv4Address('192.168.1.101'),
+		#	  'name': 'ethernet',
+		#	  'path': '/org/freedesktop/NetworkManager/Devices/1'},
+		#	 {'address': IPv4Address('192.168.12.1'),
+		#	  'name': 'usb',
+		#	  'path': '/org/freedesktop/NetworkManager/Devices/3'}]
+		
+		self.uiNetworkInterface.clear()
+		for interface in interfaces:
+			self.uiNetworkInterface.addItem(interface['name'], interface)
+		#self.uiNetworkInterface.setCurrentIndex(0)
+	
+	def updateNetworkInterfaceInformation(self, *_):
+		self.updateNetworkInterface(self.uiNetworkInterface.currentIndex())
+	
+	def updateNetworkInterface(self, index):
+		interface = self.uiNetworkInterface.itemData(index)
+		commandWhenDisabled = '-'
+		if interface:
 			self.uiNoNetworkConnection.hide()
 			self.uiNetworkInterfaceInformation.show()
 			self.uiNetworkInterface.show()
-			self.uiNetworkInterface.clear()
-			for interface in interfaces:
-				self.uiNetworkInterface.addItem(interface['name'], interface)
-			self.showNetworkInterface(0) #Update elements in uiNetworkInterfaceInformation.
+			
+			HTTPPort = f'\n :{self.uiHTTPPort.text()}' if not self.uiHTTPPort.text() == '80' else '' #don't show default ports, they're implicit
+			SSHPort = f'\n -p {self.uiSSHPort.text()}' if not self.uiSSHPort.text() == '22' else ''
+			appUrl = f"http://{interface['address']}{HTTPPort}" 
+			sshCommand = f"ssh root@{interface['address']}{SSHPort}"
+			
+			self.uiAppUrl.setText(
+				appUrl if self.uiEnableHTTP.isChecked() else commandWhenDisabled )
+			self.uiSSHCommand.setText(
+				sshCommand if self.uiEnableSSH.isChecked() else commandWhenDisabled )
+			
+			self.updateQRCodes()
+			
 		else:
 			self.uiNoNetworkConnection.show()
 			self.uiNetworkInterfaceInformation.hide()
 			self.uiNetworkInterface.hide()
+			self.uiAppUrlQRCode.hide()
+			self.uiSSHCommandQRCode.hide()
+			
+			self.uiAppUrl.setText(commandWhenDisabled)
+			self.uiSSHCommand.setText(commandWhenDisabled)
 	
-	def showNetworkInterface(self, index):
-		interface = self.uiNetworkInterface.itemData(index)
-		if interface:
-			HTTPPort = f'\n :{self.uiHTTPPort.text()}' if not self.uiHTTPPort.text() == '80' else '' #don't show default ports, they're implicit
-			SSHPort = f'\n -p {self.uiSSHPort.text()}' if not self.uiSSHPort.text() == '22' else ''
-			
-			self.uiAppUrl.setText(
-				f"https://{interface['IP']}{HTTPPort}" 
-				if self.uiEnableHTTP.isChecked() else
-				'disabled' )
-			
-			self.uiSSHCommand.setText(
-				f"ssh root@{interface['IP']}{SSHPort}" 
-				if self.uiEnableSSH.isChecked() else
-				'disabled' )
-			
-			#Use QREncode to display the URLs here.
-			
-		else:
-			self.uiAppUrl.setText('disabled')
-			self.uiSSHCommand.setText('disabled')
+	
+	def updateQRCodes(self):
+		interface = self.uiNetworkInterface.currentData()
+		HTTPPort = f'\n :{self.uiHTTPPort.text()}' if not self.uiHTTPPort.text() == '80' else '' #don't show default ports, they're implicit
+		SSHPort = f'\n -p {self.uiSSHPort.text()}' if not self.uiSSHPort.text() == '22' else ''
+		appUrl = f"http://{interface['address']}{HTTPPort}/app" 
+		sshCommand = f"ssh root@{interface['address']}{SSHPort}"
 		
+		external_process.run(self,
+			['qrencode', '-o', '-', appUrl],
+			lambda err: None,
+			lambda data: self.updateAppQRCode(self.uiAppUrlQRCode, data),
+			binaryOutput = True,
+		)
+		external_process.run(self,
+			['qrencode', '-o', '-', sshCommand],
+			lambda err: None,
+			lambda data: self.updateAppQRCode(self.uiSSHCommandQRCode, data),
+			binaryOutput = True,
+		)
+	
+	def updateAppQRCode(self, target, qrCodePng):
+		qrCodePixmap = QPixmap()
+		qrCodePixmap.loadFromData(
+			qrCodePng, 'png', QtCore.Qt.MonoOnly )
+		border = 10 #px
+		qrCodePixmap = qrCodePixmap.copy(QtCore.QRect(
+			border, border,
+			qrCodePixmap.width() - border*2, qrCodePixmap.height() - border*2
+		))
+		target.setPixmap(qrCodePixmap)
+		target.show()
+		
+	
+	
+	def setPassword(self, password):
+		log.print(f'setPassword {password}')
+		settings.setValue('password', password)
+		self.setHTTPPassword(password)
+		self.setSSHPassword(password)
+	
+	def setHTTPPassword(self, password): #password
+		webServer.setValue('password',
+			hexHash(password=password) if password else '' )
+		self.reloadHTTP()
+
+	def setSSHPassword(self, password):
+		try:
+			execSetPassword('root', password) #We should reeeeeally switch Web and SSH to someone else.
+		except Exception as e:
+			log.error(str(e))
+		self.reloadSSH()
+
+
+	def setSSH(self, *, on=True):
+		log.print(f'setSSH {on}')
+		settings.setValue('ssh enabled', on)
+			
+		if on:
+			self.uiSSHStatus.showMessage(
+				f"Status: Starting…",
+				timeout = 0 )
+			
+			external_process.run(self,
+				['systemctl', 'enable', 'ssh'],
+				
+				lambda err: (
+					self.uiSSHStatus.showError(
+						f"Status: Error. See journalctl.", 
+						timeout = 0 ),
+					log.error(err),
+				),
+				
+				lambda *_: external_process.run(self,
+					['service', 'ssh', 'start'],
+					
+					lambda err: (
+						self.uiSSHStatus.showError(
+							f"Status: Error. See journalctl.", 
+							timeout = 0 ),
+						log.error(err),
+					),
+					
+					lambda *_:
+						self.uiSSHStatus.showMessage(
+							f"Status: Running.",
+							timeout = 0 )
+				)
+			)
+		
+		else: #off
+			self.uiSSHStatus.showMessage(
+				f"Status: Stopping…",
+				timeout = 0 )
+			
+			external_process.run(self,
+				['service', 'ssh', 'stop'],
+				
+				lambda err: (
+					self.uiSSHStatus.showError(
+						f"Status: Error. See journalctl.", 
+						timeout = 0 ),
+					log.error(str(err)),
+				),
+				
+				lambda *_: external_process.run(self,
+					['systemctl', 'disable', 'ssh'],
+					
+					lambda err: (
+						self.uiSSHStatus.showError(
+							f"Status: Error. See journalctl.", 
+							timeout = 0 ),
+						log.error(str(err)),
+					),
+					
+					lambda *_:
+						self.uiSSHStatus.showMessage(
+							f"Status: Stopped.",
+							timeout = 0 )
+				)
+			)
+
+	def setSSHPort(self, num):
+		log.print(f'setSSHPort {num}')
+		settings.setValue('ssh port', num)
+		self.reloadSSH()
+	
+	def reloadSSH(self):
+		if not settings.value('ssh enabled', False):
+			return
+		
+		self.uiSSHStatus.showMessage(
+			f"Status: Working…",
+			timeout = 0 )
+		
+		external_process.run(self,
+			['service', 'ssh', 'reload'],
+			
+			lambda err: (
+				self.uiSSHStatus.showError(
+					f"Status: Error. See journalctl.", 
+					timeout = 0 ),
+				log.error(str(err)),
+			),
+			
+			lambda *_:
+				self.uiSSHStatus.showMessage(
+					f"Status: Running.",
+					timeout = 0 )
+		)
+
+
+	def setHTTP(self, *, on=True):
+		log.print(f'setHTTP {on}')
+		settings.setValue('http enabled', on)
+		
+		if on:
+			self.uiHTTPStatus.showMessage(
+				f"Status: Starting…",
+				timeout = 0 )
+			
+			external_process.run(self,
+				['systemctl', 'enable', 'chronos-web-api'],
+				
+				lambda err: (
+					self.uiHTTPStatus.showError(
+						f"Status: Error. See journalctl.", 
+						timeout = 0 ),
+					log.error(str(err)),
+				),
+				
+				lambda *_: external_process.run(self,
+					['service', 'chronos-web-api', 'start'],
+					
+					lambda err: (
+						self.uiHTTPStatus.showError(
+							f"Status: Error. See journalctl.", 
+							timeout = 0 ),
+						log.error(str(err)),
+					),
+					
+					lambda *_:
+						self.uiHTTPStatus.showMessage(
+							f"Status: Running.",
+							timeout = 0 )
+				)
+			)
+		
+		else: #off
+			self.uiHTTPStatus.showMessage(
+				f"Status: Stopping…",
+				timeout = 0 )
+			
+			external_process.run(self,
+				['service', 'chronos-web-api', 'stop'],
+				
+				lambda err: (
+					self.uiHTTPStatus.showError(
+						f"Status: Error. See journalctl.", 
+						timeout = 0 ),
+					log.error(err),
+				),
+				
+				lambda *_: external_process.run(self,
+					['systemctl', 'disable', 'chronos-web-api'],
+					
+					lambda err: (
+						self.uiHTTPStatus.showError(
+							f"Status: Error. See journalctl.", 
+							timeout = 0 ),
+						log.error(err),
+					),
+					
+					lambda *_:
+						self.uiHTTPStatus.showMessage(
+							f"Status: Stopped.",
+							timeout = 0 )
+				)
+			)
+
+	def setHTTPPort(self, num):
+		log.print(f'setHTTPPort {num}')
+		settings.setValue('http port', num)
+		webServer.setValue('port', json.dumps(num))
+		self.reloadHTTP()
+	
+	def reloadHTTP(self):
+		if not settings.value('http enabled', True):
+			return
+		
+		self.uiHTTPStatus.showMessage(
+			f"Status: Working…",
+			timeout = 0 )
+		
+		external_process.run(self,
+			['service', 'chronos-web-api', 'restart'],
+			
+			lambda err: (
+				self.uiHTTPStatus.showError(
+					f"Status: Error. See journalctl.", 
+					timeout = 0 ),
+				log.error(err),
+			),
+			
+			lambda *_:
+				self.uiHTTPStatus.showMessage(
+					f"Status: Running.",
+					timeout = 0 )
+		)
