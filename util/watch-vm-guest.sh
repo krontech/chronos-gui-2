d24022b70ddf778755495bbf9f4849c6d0390719
David Roberts <ddr@krontech.ca>
2019-12-11 13:34:35 -0800

    Ported improvements from watch-camera to watch-vm-guest.

--- util/watch-vm-guest.sh
+++ util/watch-vm-guest.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-set -uo pipefail #enable bash's safer mode
+set -euxo pipefail #enable bash's unofficial safe mode
 IFS=$'\n'
 
 #This script runs the Python-based Chronos on-board GUI.
 #When a file is changed, Python is automatically restarted.
 #brk() may be used in the Python script to enter an interactive debugger.
 
-trap "{ stty sane; echo; kill 0; }" EXIT #Kill all children when we die. (This cleans up any windows lying around.) Also restore the console (keyboard stops being echoed after ctrl-c'ing out of pdb) and advance to a new line before printing the prompt again.
+trap "{ stty sane; echo; kill 0; }" EXIT #Kill all children when we die. (This cleans up any windows lying around.) Also restore the console (keyboard stops being echoed after ctrl-c'ing out of pdb) and advance to a new line before printing the prompt again. When python is in pdb() on the camera, it will not respond to anything other than -9 unfortunately.
 
 bash <<< "#sh doesn't do the equality test for 143, must use bash
 	while true; do
 		sleep 2 &
 		python3 src/main.py < `readlink -f /dev/stdin` 2> `readlink -f /dev/stderr`
 		PY_EXIT=\$?
-		[[ \$PY_EXIT -eq 143 ]] || echo Python exited with code \$PY_EXIT. Restarting... #Python exits with 143 when killed by entr running pkill. We don't really care about that, since it's so frequent, but knowing when it's died of other causes is useful.
+		[[ \$PY_EXIT -eq 137 ]] || echo Python exited with code \$PY_EXIT. Waiting… #Python exits with 137 when killed by watchdog running pkill. We don't really care about that, since it's so frequent, but knowing when it's died of other causes is useful.
 		wait #In combination with sleep 2, don't restart the python script until at least two seconds have passed since the last invocation. This stops python from running many times if python crashes immediately.
 		echo; echo ----- reset -----; echo; #Visually separate logs.
 	done
@@ -22,6 +22,7 @@ PYTHON_PARENT_SHELL=$! #Used to limit pkill to the subshell we're running our py
 
 #Watch for filesystem changes and run a command. If another change comes in during the timeout period, ignore the first change.
 python3 - $PYTHON_PARENT_SHELL << 'EOL'
+# -*- coding: future_fstrings -*-
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 import subprocess, sys
@@ -31,10 +32,11 @@ timeout = 0.05 #seconds
 killingProcess = None
 
 def callback(evt):
+	"""Terminate running python in this session, so the bash loop above restarts it."""
 	global killingProcess
 	killingProcess and killingProcess.kill()
-	"""Terminate running python in this session, so the bash loop above restarts it."""
-	killingProcess = subprocess.Popen(f"sleep {timeout} && pkill -P {sys.argv[1]} -f main.py", shell=True)
+	# SIGKILL is needed only on the camera, as pdb() does not respond to SIGTERM only on the camera.
+	killingProcess = subprocess.Popen(f"sleep {timeout} && pkill --signal SIGKILL --parent {sys.argv[1]} --full main.py", shell=True)
 	
 event_handler = PatternMatchingEventHandler(
 	patterns=["*.py","*.ui","*.svg","*.png"],
