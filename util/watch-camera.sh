fcea722fe8e57351e601481fa1fc07765a4d7703
David Roberts <ddr@krontech.ca>
2020-01-07 17:31:40 -0800

    Camera watcher gained the ability to pass through args to Python.

--- util/watch-camera.sh
+++ util/watch-camera.sh
@@ -22,7 +22,7 @@ bash <<< "#sh doesn't do the equality test for 143, must use bash
 	
 	while true; do
 		sleep 2 &
-		PYTHONPATH="." python3 chronosGui2/__main__.py < `readlink -f /dev/stdin` 2> `readlink -f /dev/stderr`
+		PYTHONPATH="." python3 chronosGui2/__main__.py $@ < `readlink -f /dev/stdin` 2> `readlink -f /dev/stderr`
 		PY_EXIT=\$?
 		[[ \$PY_EXIT -eq 137 ]] || echo Python exited with code \$PY_EXIT. Waiting… #Python exits with 137 when killed by watchdog running pkill. We don't really care about that, since it's so frequent, but knowing when it's died of other causes is useful.
 		wait #In combination with sleep 2, don't restart the python script until at least two seconds have passed since the last invocation. This stops python from running many times if python crashes immediately.
